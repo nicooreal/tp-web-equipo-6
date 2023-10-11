6a280244355a5c8a2e1eac77fa2cb19a379ef5fc
unknown <macchimateo@gmail.com>
2023-10-10 21:19:03 -0300

    Se crea Clase Carrito y CarritoNeogcio completas

---
+++ Dominio/Carrito.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+
+    public class Carrito
+    {
+
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public string marca { get; set; }
+        public float precio { get; set; }
+        //public string url { get; set; }
+
+
+    }
+}
---
+++ Negocio/CarritoNegocio.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace Negocio
+{
+    public class CarritoNegocio
+    {
+        public List<Carrito> listaCarrito = new List<Carrito>();
+
+        public List<Carrito> listar()
+        {
+
+
+            return listaCarrito;
+        }
+
+
+
+
+        public void AgregarAlCarrito(int id)
+        {
+            ArticuloNegocio articuloNegocio = new ArticuloNegocio();
+            AccesoDatos accesoDatos = new AccesoDatos();
+            Carrito carrito = new Carrito();
+
+            try
+            {
+                listaCarrito = CargarLista(id);
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public List<Carrito> CargarLista(int id)
+        {
+            List<Carrito> lista = new List<Carrito>();
+
+            try
+            {
+
+
+
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+    }
+}
