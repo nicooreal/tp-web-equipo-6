383ffde3c0a405237805eab9de535c9d883c9a3d
unknown <macchimateo@gmail.com>
2023-10-13 00:18:00 -0300

    Se crea clase Sesion y webform con masterpage Carrito.aspx. Falta desarrollar

---
+++ WebApplication1/Carrito.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+using Dominio;
+using System.Drawing.Imaging;
+
+namespace CarritoDeCompras
+{
+    public partial class Carrito : System.Web.UI.Page
+    {
+
+        public List<Articulo> ListaArticulo { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Sesion sesion = new Sesion();
+
+            RepCarrito.DataSource = sesion.ListadeCarrito();
+            RepCarrito.DataBind();
+
+            float total = 0;
+
+            foreach (var item in sesion.ListadeCarrito())
+            {
+                total += item.precio;
+            }
+
+            lblTotal.Text = total.ToString();
+
+
+        }
--- WebApplication1/Default.aspx
+++ WebApplication1/Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/maestro.Master" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="WebApplication1.login" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/maestro.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1.login" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
